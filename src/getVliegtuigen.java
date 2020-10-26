74fd408b5f7aefe1a2e1191785f4785cddd9e85c
Dlankheet <daniel.lankheet@hotmail.com>
2020-10-26 10:44:51 +0100

    Script werkt, meuk toegevoegd.

--- src/getVliegtuigen.java
+++ src/getVliegtuigen.java
@@ -1,37 +1,71 @@
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
 import java.util.Iterator;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class getVliegtuigen {
-	public static void main(String[] args){
-		try
-		{
+	public static void main(String[] args) {
+		try {
 			File file = new File("C:\\Users\\Daniel\\Desktop\\planes.xlsx");
 			FileInputStream fis = new FileInputStream(file);
 			XSSFWorkbook wb = new XSSFWorkbook(fis);
 			XSSFSheet sheet = wb.getSheetAt(0);
 			Iterator<Row> itr = sheet.iterator();
 
-			int target = 400;
-			int i=0;
+			int target = 100;
+			int i = 0;
 			// Skip de eerste row
 			Row row = itr.next();
-			while(i<target){
+			String flightinfo = "VALUES";
+			while (i < target) {
 				row = itr.next();
-//
-//								row.getCell(0).getStringCellValue(),
-//								row.getCell(7).getStringCellValue()
-;
+				String info = String.format("('%s', %s), \n",
+						row.getCell(0).getStringCellValue(),
+						i);
+
+						//row.getCell(7).getStringCellValue());
+				flightinfo = flightinfo + info;
 				i++;
 			}
-		}
-		catch(Exception e)
-		{
+			writeFile("test.sql", flightinfo);
+		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	public static String readFile(String filename) throws IOException {
+		File file = new File(filename);
+		int len = (int) file.length();
+		byte[] bytes = new byte[len];
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(file);
+			assert len == fis.read(bytes);
+		} catch (IOException e) {
+			close(fis);
+			throw e;
+		}
+		return new String(bytes, "UTF-8");
+	}
+
+	public static void writeFile(String filename, String text) throws IOException {
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(filename);
+			fos.write(text.getBytes("UTF-8"));
+		} catch (IOException e) {
+			close(fos);
+			throw e;
+		}
+	}
+
+	public static void close(Closeable closeable) {
+		try {
+			closeable.close();
+		} catch (IOException ignored) {
 		}
 	}
 }
